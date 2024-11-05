5f270a6c3fdfdd086fa40366cf6c32cbcd69360e
Jean Henry <jean.henry@ansys.com>
2024-11-05 22:13:21 +0100

    Add case rules

---
+++ doc/source/rules/case/enum_has_default_case.rst
@@ -0,0 +1,39 @@
+.. index:: single: Enum Has Default Case
+
+Enum Has Default Case
+=====================
+
+.. rule::
+   :filename: enum_has_default_case.py
+   :class: EnumHasDefaultCase
+   :id: id_0017
+   :reference: n/a
+   :kind: generic
+   :tags: case
+
+   Enum default checked
+
+Description
+-----------
+
+.. start_description
+
+In the SCADE model, each enumeration shall use the 'default' for one element to catch any abnormal value.
+
+.. end_description
+
+Rationale
+---------
+This ensures a defensive design by building abnormal values into every enumeration, promoting correct abnormal value management by all model entities using the enumeration.
+
+Verification
+------------
+This rule iterates over enumeration definitions and checks that at least one element bears pragma text 'default'.
+
+Resolution
+----------
+Modify the offending enumeration to add a ``default`` definition, either with one of the "normal" values (such as ``Hold``), or a specifically added value (such as ``Abnormal``).
+
+Customization
+-------------
+N/A.
---
+++ tests/case/test_enum_has_default_case.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2021 - 2024 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import pytest
+import scade.model.suite as suite
+
+from ansys.scade.design_rules.case.enum_has_default_case import EnumHasDefaultCase
+from ansys.scade.design_rules.utils.rule import Rule
+from tests.conftest import load_session
+
+# shorter names
+_OK = Rule.OK
+_FAILED = Rule.FAILED
+_ERROR = Rule.ERROR
+_NA = Rule.NA
+
+
+@pytest.fixture(scope='session')
+def model():
+    """Unique instance of the test model EnumHasDefaultCase."""
+    pathname = 'tests/case/EnumHasDefaultCase/EnumHasDefaultCase.etp'
+    return load_session(pathname).model
+
+
+@pytest.mark.parametrize(
+    'path, expected',
+    [
+        ('P::EnumOk/', _OK),
+        ('P::EnumAllOk/', _OK),
+        ('P::EnumNok/', _FAILED),
+        ('P::EnumNoKcgNok/', _FAILED),
+    ],
+)
+def test_enum_has_default_case(model: suite.Model, path: str, expected: int):
+    element = model.get_object_from_path(path)
+    assert element
+    rule = EnumHasDefaultCase()
+    status = rule.on_check(element.type)
+    assert status == expected
