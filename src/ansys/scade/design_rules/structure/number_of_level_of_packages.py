00ff9c30e21e211a0002b7b43d6db6bb57beb494
Romain Andrieux <romain.andrieux@ansys.com>
2024-11-18 15:55:33 +0100

    Add pragma no cover to "if __name__ == '__main__':" statements

--- src/ansys/scade/design_rules/structure/number_of_level_of_packages.py
+++ src/ansys/scade/design_rules/structure/number_of_level_of_packages.py
@@ -24,7 +24,7 @@
 
 """Implements the NumberOfLevelOfPackages rule."""
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: no cover
     # rule instantiated outside of a package
     from os.path import abspath, dirname
     import sys
@@ -84,6 +84,6 @@ class NumberOfLevelOfPackages(Rule):
         return level + 1
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: no cover
     # rule instantiated outside of a package
     NumberOfLevelOfPackages()
