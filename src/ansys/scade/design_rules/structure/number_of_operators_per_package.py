082d71ca68d7719806d9912ca5cb16bf75558669
Romain Andrieux <romain.andrieux@ansys.com>
2024-11-18 15:35:08 +0100

    Add pragma no cover to "if __name__ == '__main__':" statements

--- src/ansys/scade/design_rules/structure/number_of_operators_per_package.py
+++ src/ansys/scade/design_rules/structure/number_of_operators_per_package.py
@@ -24,7 +24,7 @@
 
 """Implements the NumberOfOperatorsPerPackage rule."""
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: no cover
     # rule instantiated outside of a package
     from os.path import abspath, dirname
     import sys
@@ -83,6 +83,6 @@ class NumberOfOperatorsPerPackage(Rule):
         return Rule.OK
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: no cover
     # rule instantiated outside of a package
     NumberOfOperatorsPerPackage()
