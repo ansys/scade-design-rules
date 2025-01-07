2b447a9760925a78729b5d52a437a668d972849c
Jean Henry <jean.henry@ansys.com>
2025-01-07 10:55:27 +0100

    chore: Add pragma `no cover`

--- src/ansys/scade/design_rules/structure/maximum_user_ops_in_diagram.py
+++ src/ansys/scade/design_rules/structure/maximum_user_ops_in_diagram.py
@@ -24,7 +24,7 @@
 
 """Implements the MaximumUserOpsInDiagram rule."""
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: no cover
     # rule instantiated outside of a package
     from os.path import abspath, dirname
     import sys
@@ -85,6 +85,6 @@ class MaximumUserOpsInDiagram(Rule):
         return Rule.OK
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: no cover
     # rule instantiated outside of a package
     MaximumUserOpsInDiagram()
